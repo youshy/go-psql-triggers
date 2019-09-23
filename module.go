8ad49ea366b7330a35a3fc5f68e0a3808bac209c
youshy <devilyoushy@gmail.com>
2019-09-23 17:23:22 +0100

    Copied buidl - waiting for psql setup

---
+++ module.go
@@ -0,0 +1,20 @@
+package main
+
+// Setup required for C build
+
+/*
+#include "postgres.h"
+#include "fmgr.h"
+
+#cgo LDFLAGS: -Wl,--unresolved-symbols=ignore-all
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+PG_FUNCTION_INFO_V1(mytrigger);
+*/
+import "C"
+
+func main() {
+}
